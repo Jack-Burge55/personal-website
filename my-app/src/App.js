c1ab89c4bea00bbab5617d2b1c8f5a4d68a4f06b
Jack Burgess <jack.burgess412@gmail.com>
2023-02-08 20:18:08 +0000

    creating components, error in switch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Switch from "react-router-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Navbar from './components/Navbar'
 import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
+  const [testState, setTestState] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTestState(testState + 1);
+    }, 1000);
+  }, [testState]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="*" component={() => <h2>404 Not Found </h2>} />
+      </Switch>
+      <h1>Welcome to my page</h1>
+      <h2>You've been here for {testState} seconds</h2>
     </div>
   );
 }
