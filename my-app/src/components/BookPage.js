7d5c69f37767ec49d6d7bf8e7b00b3b11cb502e9
Jack Burgess <jack.burgess412@gmail.com>
2023-02-23 20:42:11 +0000

    improved bookPage styling, add paragraphs and quote

--- my-app/src/components/BookPage.js
+++ my-app/src/components/BookPage.js
@@ -6,17 +6,32 @@ const BookPage = () => {
     const details = booksObject["Books"][bookId]
     return(
         <>
-        <img className='bookImage' alt={"SF Masterworks cover"} src={details.Image} />
-        <h2>{details.Title}</h2>
-        <h3>{details.Author}</h3>
-        <h4>{"⭐".repeat(details.Rating)} ({(details.Rating)}/10)</h4>
-        <br/>
-        <p>{details.Review}</p>
-        <ul>
-            {details.Themes.map((theme, id) => {
-                return <li key={id}>{theme}</li>
-            })}
-        </ul>
+        <div className='bookPageDiv'>
+            <div className='bookPageDetails'>
+                <h2>{details.Title}</h2>
+                <h3>{details.Author}</h3>
+                <h4>{"⭐".repeat(details.Rating)} ({(details.Rating)}/10)</h4>
+                <br/>
+                <p>{details.Blurb}</p>
+                <ul>
+                    {details.Themes.map((theme, id) => {
+                        return <li key={id}>{theme}</li>
+                    })}
+                </ul>
+            </div>
+            <div className='bookPageImageDiv'>
+                <img className='bookPageImage' alt={"SF Masterworks cover"} src={details.Image} />
+            </div>
+        </div>
+
+        <p className='bookPageQuote'>{details.Quote}</p>
+        {Object.keys(details.Review).map((paragraphKey, id) => {
+            return (
+            <div key={id}>
+                <p>{details.Review[paragraphKey]}</p>
+            </div>
+            )
+        })}
         </>
     )
 }
--- my-app/src/components/BookTile.js
+++ my-app/src/components/BookTile.js
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom';
 const BookTile = (singleBookObject) => {
     return (
         <Link to={`/book/${singleBookObject.id}`}>
-            <div className='bookSquare'>
+            <div className='bookTileSquare'>
             <h3>{singleBookObject.details.Title}</h3>
-            <img className='bookImage' src={singleBookObject.details.Image} />
+            <img className='bookTileImage' src={singleBookObject.details.Image} />
             </div>
         </Link>
     )
